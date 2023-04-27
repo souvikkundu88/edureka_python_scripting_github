e09e27c7b940487263dd5fc115c016ce2fa03679
Souvik Kundu <kivtech88@gmail.com>
2023-04-27 23:50:17 +0530

    Update assignment3_solution_module1.py

--- module1_assignment_solution/assignment3_solution_module1.py
+++ module1_assignment_solution/assignment3_solution_module1.py
@@ -6,15 +6,16 @@ Hint: In case of input data being supplied to the question, it should be assumed
 be a console input. Divide each digit with 2 and verify is it even or not.
 '''
 
-'''
+
 even_nums = []
 for i in range(1000, 3001):
     num = str(i)
     if int(num[0]) % 2 == 0 and int(num[1]) % 2 == 0 and int(num[2]) % 2 == 0 and int(num[3]) % 2 == 0:
         even_nums.append(num)
 print(",".join(even_nums))
-'''
 
+
+'''
 result = []
 for num in range(1000, 3001):
     # Check if all digits of the number are even
@@ -23,3 +24,6 @@ for num in range(1000, 3001):
         result.append(str(num))
 # Print the comma-separated sequence of even-digit numbers
 print(",".join(result))
+
+'''
+
