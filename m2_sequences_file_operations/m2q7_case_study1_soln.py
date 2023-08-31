3e88ea4bcfa0bc2a5fbfe199ab1d535f15176e87
Souvik Kundu <kivtech88@gmail.com>
2023-08-31 21:25:25 +0530

    Create m2q7_case_study1_soln.py

---
+++ m2_sequences_file_operations/m2q7_case_study1_soln.py
@@ -0,0 +1,26 @@
+'''
+Question 7
+Please write a program which count and print the numbers of each character in a
+string input by console.
+Example: If the following string is given as input to the program:
+abcdefgabc
+Then, the output of the program should be:
+a,2
+c,2
+b,2
+e,1
+d,1
+g,1
+f,1
+'''
+input_string = input("Enter a string: ")
+char_count = {}
+
+for char in input_string:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1
+
+for char, count in char_count.items():
+    print(f"{char},{count}")
