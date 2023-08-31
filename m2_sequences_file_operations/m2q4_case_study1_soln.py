a58d06fb557e615c64cdbbc79dfae1b45c721b30
Souvik Kundu <kivtech88@gmail.com>
2023-08-31 21:11:47 +0530

    commit

---
+++ m2_sequences_file_operations/m2q4_case_study1_soln.py
@@ -0,0 +1,9 @@
+'''
+Write a for loop that prints all elements of a list and their position in the list.
+a = [4,7,3,2,5,9]
+Hint: Use Loop to iterate through list elements.
+'''
+a = [4, 7, 3, 2, 5, 9]
+
+for index, value in enumerate(a):
+    print(f"Element at position {index} is: {value}")
